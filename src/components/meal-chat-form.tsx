1b47a1d0b59b66bd866ff6291448fcf81528f843
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-27 12:12:13 -0300

    feat: integrating vercel ai sdk

--- src/components/meal-chat-form.tsx
+++ src/components/meal-chat-form.tsx
@@ -1,35 +1,72 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react'
 import { Label } from './ui/label'
 import { UtensilsCrossed } from 'lucide-react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import PromptSelect from './prompt-select'
+import { useCompletion } from 'ai/react'
 
 const MealChatForm = () => {
+  const promptRef = useRef<HTMLInputElement>(null)
+
+  function handleInputPrompt(): string | undefined {
+    const prompt = promptRef.current?.value
+    return prompt
+  }
+
+  const {
+    setInput,
+    input,
+    completion,
+    handleSubmit
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/completion',
+    body: {
+      prompt: handleInputPrompt()
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+
   return (
-    <div className='flex gap-8'>
-      <form className="space-y-6 w-full">
-        <div className='space-y-4'>
+    <div className='flex flex-col gap-8'>
+
+      {/* chat form */}
+      <div className='flex gap-6'>
+        <form onSubmit={handleSubmit} className='space-y-4 w-full'>
           <div>
             <Label>Ingredientes</Label>
+
             <Input
-              placeholder="Digite os ingredientes da sua refeição separados por vírgula"
-              name='ingredients'
+              placeholder='Insira os ingredientes que deseja na receita, separados por vírgula'
+              ref={promptRef}
             />
           </div>
 
-          <PromptSelect />
-        </div>
+          <PromptSelect onPromptSelected={setInput} />
 
-        <Button className="gap-1 w-full" variant='outline'>
-          Executar
-          <UtensilsCrossed className="h-4 w-4" />
-        </Button>
-      </form >
+          <Button type="submit" className='gap-1'>
+            Executar
+            <UtensilsCrossed className='w-4 h-4' />
+          </Button>
+        </form>
 
+        <Textarea
+          placeholder='Preview da instrução para IA...'
+          value={input}
+        />
+      </div>
+
+      {/* ai textarea response */}
       <Textarea
-        placeholder="Resposta da IA..."
+        placeholder='Resposta da IA...'
+        className='leading-relaxed h-72 resize-none'
+        readOnly
+        value={completion}
       />
     </div>
   )
